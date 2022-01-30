c1c38cb2614caad662dd689a864ea38d344ceb23
Nikita Popov <popov.n294@gmail.com>
2022-01-30 19:56:28 +0300

    Relative jacobian added (not tested)

--- relative_jacobian.py
+++ relative_jacobian.py
@@ -6,7 +6,13 @@ def relative_jacobian_matrix(left_T_mat, right_T_mat, left_J, right_J, y_positio
     relative_jacobian = np.array(np.zeros((6,6)))
 
     relative_jacobian[:3,:3] = right_T_mat[:3,:3].T 
-    relative_jacobian[3:, 3:] = right_T_mat[:3,:3].T 
+    relative_jacobian[3:, 3:] = right_T_mat[:3,:3].T
+    left_gamma = __gamma_matrix(left_T_mat)
+    right_gamma = __gamma_matrix(right_T_mat)
+    gamma_array = np.array([left_gamma.dot(left_J), -right_gamma.dot(right_J)]) 
+    
+    relative_jacobian = relative_jacobian.dot(gamma_array[0]) - relative_jacobian.dot(gamma_array[1])
+
 
 
     return relative_jacobian
